9393852d741d3204d94cb462db3472cb1297afde
TurtleMaster69420 <48506404+TurtleMaster69420@users.noreply.github.com>
2019-04-22 20:43:31 +1000

    Update mockify.py

--- mockify.py
+++ mockify.py
@@ -1,4 +1,10 @@
+# Now you can mock people... without having to manually making each letter lowercase or uppercase! How revolutionary (I bet no one has ever done this)
+# And it's free! That's a great price!
+
+
 from random import randint
+
+
 while True:
     string = input("I want to mockify the following: ")
     newstring = []
