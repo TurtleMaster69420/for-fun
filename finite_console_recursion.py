b2563d3de6be1d3d211f916509728999ba4e3504
yellowsubmarine1447 <48506404+yellowsubmarine1447@users.noreply.github.com>
2023-12-16 22:04:20 +1100

    my previous code was bad idk why

--- finite_console_recursion.py
+++ finite_console_recursion.py
@@ -1,23 +1,22 @@
+# could maybe make better by getting parent command used to run current process and saying "run that" but whatever
+
 import sys
-import time
-import os
 import subprocess
+import time
+
+TIME_BUFFER = 0.2
+SLEEP_TIME = 4
+
 if len(sys.argv) == 1:
-    print("Enter limit: ")
-    limit = int(input())
-    file_id = 1
+    limit = int(input("Enter number of consoles: "))
 else:
-    limit = int(sys.argv[2])
-    if limit == 0:
-        time.sleep(4)
-        quit()
-    file_id = int(sys.argv[1])
-    print(file_id)  
+    limit = int(sys.argv[1])
 
-with open(f"finite_file_recursion{file_id}.py") as f:
-    with open(f"finite_file_recursion{file_id + 1}.py", "w") as g:
-        g.write(f.read())
+print(limit)
 
-
-print("spam")
-subprocess.run(f"python finite_file_recursion{file_id}.py {file_id + 1} {limit-1}", creationflags=subprocess.CREATE_NEW_CONSOLE)
+if limit:
+    time.sleep(TIME_BUFFER)
+    subprocess.Popen(f"python {__file__} {limit-1}", creationflags=subprocess.CREATE_NEW_CONSOLE)
+    # hydra mode, will create 2**n consoles
+    #subprocess.Popen(f"python {__file__} {limit-1}", creationflags=subprocess.CREATE_NEW_CONSOLE)
+time.sleep(SLEEP_TIME)
